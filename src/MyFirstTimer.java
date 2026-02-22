0b6f7ba60ac0ba7d772327340b8baaea9e4c6b9a
Darrel Mbanefo <d_mbanefo@u.pacific.edu>
2026-02-22 15:43:30 -0800

    A timer object for how often it wakes up and who to notify An action Listener define action performed Starting the timer because creating it won't start it u need t.start();

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,15 @@
 import acm.graphics.*;
 import acm.program.*;
+import java.awt.event.*;
+import javax.swing.Timer;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private Timer t;
+	private int numTimes = 0;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -15,6 +19,20 @@ public class MyFirstTimer extends GraphicsProgram {
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		
+		t = new Timer(1000, this);
+		t.setInitialDelay(3000);
+		t.start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.move(5,  0);;
+		myLabel.setLabel("times called? " + numTimes);
+		
+		if (numTimes >= MAX_STEPS) {
+			t.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
